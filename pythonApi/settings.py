e13234bdf3102fc9a927a14aab63dc279c81ad15
V.Paparidis <v.paparidis@gmail.com>
2024-02-18 00:22:05 +0200

    fix settings

--- pythonApi/settings.py
+++ pythonApi/settings.py
@@ -25,14 +25,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = os.environ.get('DEBUG')
-if not DEBUG:
-    DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+
 
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    '.vercel.app'
+    '.onrender.com',
 ]
 
 
