7be3374cf8e0c02c21f8789fd23c7b763878fca2
V.Paparidis <v.paparidis@gmail.com>
2024-02-18 01:17:46 +0200

    fix database

--- pythonApi/models.py
+++ pythonApi/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class UserProfile(models.Model):
     username = models.CharField(max_length=100)
-    url = models.CharField(max_length=200)
+    url = models.CharField(max_length=200, default='', blank=True, null=True)
     followersCount = models.IntegerField()
     followsCount = models.IntegerField()
-    profilePicUrl = models.CharField(max_length=200)
+    profilePicUrl = models.CharField(max_length=200, default='', blank=True, null=True)
 
     def __str__(self):
         return self.username
